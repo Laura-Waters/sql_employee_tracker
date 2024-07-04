e5e2b784de4c93338f9209078d40bfb380b6c7ee
Laura <laurawatersalston@gmail.com>
2024-07-03 20:51:42 -0400

    inserts departments table

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 // DEPENDENCIES 
 const inquirer = require('inquirer'); 
 const fs = require('fs'); 
+const { Pool } = require('pg');
 
+// DATA/DATABASES 
+const pool = new Pool(
+  {
+      user: 'postgres',
+      password: 'laura29',
+      host: 'localhost',
+      database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+)
+
+pool.connect();   
 
 inquirer
   .prompt([
     {
-      type: 'checkbox',
+      type: 'list',
       message: ('What would you like to do?'),
       name: 'options',
       choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role'],
@@ -25,20 +38,18 @@ inquirer
       },
   ])
   .then((answers) => {
-    answers.options.forEach((option) => {
-        switch (option) {
-            case 'View All Departments':
-                pool.query('SELECT * FROM department', (err, res) => {
-                    if (err) {
-                        console.error('Error viewing all departments:', err);
-                    } else {
-                        console.log('All departments:', res.rows);
-                    }
-                });
-                break;
-            // Add cases for other options as needed
+    switch (answers.options) {
+      case 'View All Departments':
+          pool.query('SELECT * FROM departments', (err, res) => {
+              if (err) {
+                  console.error('Error viewing all departments:', err);
+              } else {
+                  console.log('All departments:', res.rows);
+              }
+          });
+          break;
+            
         }
-    });
   });
 
     // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
